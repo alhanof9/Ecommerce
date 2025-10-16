7246bde70b54ab75f6b16264ff7f0c206cef5c96
alhanof9 <hanoofaaa00@gmail.com>
2025-10-16 00:09:31 -0700

    prodect page

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'makeup',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- makeup/models.py
+++ makeup/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class prodect(models.Model):
+    name=models.CharField(max_length=200)
+    desc=models.TextField()
+    price=models.IntegerField()
+    idP=models.IntegerField()
+    img=models.ImageField(upload_to="makeup/images",default="")
+
+
+class clint(models.Model):
+    name=models.CharField(max_length=200)
+    idC=models.IntegerField()
+    pas=models.CharField(max_length=200)
+
+class order(models.Model):    
+    idO=models.IntegerField()
+    amount=models.IntegerField()
+    idC=models.IntegerField()
+    idP=models.IntegerField()
+
