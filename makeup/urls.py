fc9f6138b0e418b0ae55831357141257f8fb2997
alhanof9 <hanoofaaa00@gmail.com>
2025-11-21 14:59:04 -0800

    Resolve merge conflict in base.html

--- makeup/urls.py
+++ makeup/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("", views.index, name="index"),
+   
     path("prodect" , views.prodect, name="prodect"),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
-    path("login/", views.login_view, name="login"),    
+    path("", views.login_view, name="login"),    
     path("logout/", views.logout_view, name="logout"), 
     path("register/", views.register, name="register"),
     path("basket/", views.basket, name="basket"),
